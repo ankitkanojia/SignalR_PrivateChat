c8759d39b1a690644bed6e148a5a03df41abe399
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-03-13 11:17:39 +0530

    Intitialize signalR configurations and design for chat

---
+++ SignarRChat/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SignarRChat.Startup))]
+namespace SignarRChat
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
